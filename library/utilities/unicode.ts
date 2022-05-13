0b6ed7ddc23ce1faa4f1262bb244a37215821c8f
ntabucejo <ntabucejo@gmail.com>
2022-05-13 21:58:05 +0800

    Update code to 4 layer

--- library/utilities/unicode.ts
+++ library/utilities/unicode.ts
@@ -1,10 +1,12 @@
-import cryptoRandomString from 'crypto-random-string';
+import cryptoRandomString from 'crypto-random-string'
 
 const unicode = () => {
-  const first = cryptoRandomString({length: 4, type: 'distinguishable'})
-  const last = cryptoRandomString({length: 4, type: 'distinguishable'})
-  
-  return `${first}-${last}`
+  const one = cryptoRandomString({ length: 4, type: 'distinguishable' })
+  const two = cryptoRandomString({ length: 4, type: 'distinguishable' })
+  const three = cryptoRandomString({ length: 4, type: 'distinguishable' })
+  const four = cryptoRandomString({ length: 4, type: 'distinguishable' })
+
+  return `${one}-${two}-${three}-${four}`
 }
 
 export default unicode
