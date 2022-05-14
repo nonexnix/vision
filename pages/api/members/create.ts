e4082de3c55d99a8982f392c1210d0a943f1de58
ntabucejo <ntabucejo@gmail.com>
2022-05-14 17:45:06 +0800

    Add create member

---
+++ pages/api/members/create.ts
@@ -0,0 +1,40 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import phase from '../../../library/utilities/phase'
+import postman from '../../../library/utilities/postman'
+import prisma from '../../../library/utilities/prisma'
+
+type THandler = (request: NextApiRequest, response: NextApiResponse) => void
+
+const handler: THandler = async (request, response) => {
+  if (request.method === 'POST') {
+    const body = JSON.parse(request.body)
+    try {
+      const roles = await prisma.role.findMany({
+        where: {
+          projectId: body.projectId,
+        },
+      })
+      for (const { userId, projectId } of body.members) {
+        await prisma.member.create({
+          data: {
+            userId: userId,
+            projectId: projectId,
+            authorizations: {
+              create: {
+                roleId: roles.filter((role) => role.name === 'Member')[0].id,
+              },
+            },
+          },
+        })
+      }
+      response.status(201).json(postman(201))
+    } catch (error) {
+      console.error(error)
+      response.status(500).json(postman(500))
+    }
+  } else {
+    response.status(400).json(postman(400))
+  }
+}
+
+export default handler
