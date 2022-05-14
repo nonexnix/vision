416c9632176b2a18bd3f7084ba67a9ec7d1c5d86
ntabucejo <ntabucejo@gmail.com>
2022-05-15 01:16:48 +0800

    Add message delete update api

---
+++ pages/api/messages/delete.ts
@@ -0,0 +1,22 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import postman from '../../../library/utilities/postman'
+import prisma from '../../../library/utilities/prisma'
+
+type THandler = (request: NextApiRequest, response: NextApiResponse) => void
+
+const handler: THandler = async (request, response) => {
+  if (request.method === 'DELETE') {
+    const body = JSON.parse(request.body)
+    try {
+      await prisma.message.delete({ where: { id: body.id } })
+      response.status(200).json(postman(200))
+    } catch (error) {
+      console.error(error)
+      response.status(500).json(postman(500))
+    }
+  } else {
+    response.status(400).json(postman(400))
+  }
+}
+
+export default handler
---
+++ pages/api/messages/update.ts
@@ -0,0 +1,27 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import phase from '../../../library/utilities/phase'
+import postman from '../../../library/utilities/postman'
+import prisma from '../../../library/utilities/prisma'
+
+type THandler = (request: NextApiRequest, response: NextApiResponse) => void
+
+const handler: THandler = async (request, response) => {
+  if (request.method === 'PUT') {
+    const body = JSON.parse(request.body)
+    const value = body.key === 'dueAt' ? phase(body.value, 'iso') : body.value
+    try {
+      await prisma.message.update({
+        where: { id: body.id },
+        data: { [body.key]: value },
+      })
+      response.status(200).json(postman(200))
+    } catch (error) {
+      console.error(error)
+      response.status(500).json(postman(500))
+    }
+  } else {
+    response.status(400).json(postman(400))
+  }
+}
+
+export default handler
