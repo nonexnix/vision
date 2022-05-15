5a2c18edf191f73ae83978084ffd4cfcfa1eaf05
ntabucejo <ntabucejo@gmail.com>
2022-05-15 16:18:08 +0800

    Add nextjs document

---
+++ pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+const Document = () => {
+  return (
+    <Html lang="en">
+      <Head />
+      <body className="font-primary text-slate-800 bg-slate-200">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
+
+export default Document
