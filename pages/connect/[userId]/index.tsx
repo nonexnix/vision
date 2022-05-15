2cc67054723d0e48c12cc1929f03935e2ac572c1
ntabucejo <ntabucejo@gmail.com>
2022-05-16 02:35:50 +0800

    Cleanup

--- pages/connect/[userId]/index.tsx
+++ pages/connect/[userId]/index.tsx
@@ -44,19 +44,10 @@ export const getServerSideProps: GetServerSideProps = async ({ query }) => {
     include: {
       members: {
         include: {
-          _count: {
-            select: {
-              tasks: true,
-            },
-          },
+          _count: { select: { tasks: true } },
           project: {
             include: {
-              _count: {
-                select: {
-                  members: true,
-                  tasks: true,
-                },
-              },
+              _count: { select: { members: true, tasks: true } },
             },
           },
         },
