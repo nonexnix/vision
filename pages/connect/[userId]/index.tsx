76c748b325d33e168e7207d69508d50dceba236a
ntabucejo <ntabucejo@gmail.com>
2022-05-17 00:02:46 +0800

    Remove testing

--- pages/connect/[userId]/index.tsx
+++ pages/connect/[userId]/index.tsx
@@ -8,39 +8,9 @@ import type { IUser } from '../../../library/schemas/interfaces'
 import useClientStore from '../../../library/stores/client'
 import objectified from '../../../library/utilities/objectified'
 import prisma from '../../../library/utilities/prisma'
-import unicode from '../../../library/utilities/unicode'
 
 interface IProps {
   initialUser: IUser
-}
-
-const Test = () => {
-  const user = useClientStore<IUser>((state) => state.user)
-  const createProject = useClientStore((state) => state.create.project)
-
-  const handleCreateProject = () => {
-    createProject({
-      name: `Project ${unicode()}`,
-      description: 'Project description',
-      dueAt: 'September 22, 2022',
-      userId: user.id,
-    })
-  }
-
-  return (
-    <div className="space-y-5">
-      <button className="px-3 py-2 bg-orange-500 text-white rounded" onClick={handleCreateProject}>
-        Add Project
-      </button>
-      <div className="space-y-5">
-        {user.members?.map((member) => (
-          <div className="px-3 py-2 bg-pink-500 text-white rounded" key={member.id}>
-            {member.project!.name}
-          </div>
-        ))}
-      </div>
-    </div>
-  )
 }
 
 const Home: NextPage<IProps> = ({ initialUser }) => {
@@ -60,7 +30,6 @@ const Home: NextPage<IProps> = ({ initialUser }) => {
         <Header />
         <Main>
           <section>Home Page</section>
-          <Test />
         </Main>
       </Layout>
     </Page>
