ce3e9992b3b99888a2e080a6e5f612eda78f104e
ntabucejo <ntabucejo@gmail.com>
2022-05-16 19:10:49 +0800

    Add suspense

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Suspense } from 'react'
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
+  return (
+    <Suspense fallback={<>Loading...</>}>
+      <Component {...pageProps} />
+    </Suspense>
+  )
 }
 
 export default App
