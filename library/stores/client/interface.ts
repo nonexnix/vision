c187a4458e3ec8d556594483709653994a87a59a
ntabucejo <ntabucejo@gmail.com>
2022-05-16 15:34:26 +0800

    Fix bug

--- library/stores/client/interface.ts
+++ library/stores/client/interface.ts
@@ -23,7 +23,15 @@ interface ICreate {
   members: (payload: { userId: string; projectId: string }[]) => void
   project: (payload: { name: string; description: string; dueAt: string; userId: string }) => void
   message: (payload: { text: string; memberId: string; projectId: string }) => void
-  task: (payload: { name: string; description: string; priority: TPriority; dueAt: string; memberId: string; projectId: string; participants: { memberId: string }[] }) => void
+  task: (payload: {
+    name: string
+    description: string
+    priority: TPriority
+    dueAt: string
+    memberId: string
+    projectId: string
+    participants: { access?: boolean; memberId: string }[]
+  }) => void
   participant: (payload: { access?: boolean; memberId: string; key: 'taskId' | 'todoId' | 'suggestionId' | 'fileId' | 'announcementId'; value: string }) => void
   participants: (payload: { access?: boolean; memberId: string; key: 'taskId' | 'todoId' | 'suggestionId' | 'fileId' | 'announcementId'; value: string }[]) => void
   ticket: (payload: { code: string; userId: string; projectId: string }) => void
