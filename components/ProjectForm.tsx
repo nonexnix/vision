40d34bfebebe10fff656a756769ddb2c022562ed
ntabucejo <ntabucejo@gmail.com>
2022-05-17 15:58:25 +0800

    Update form

--- components/ProjectForm.tsx
+++ components/ProjectForm.tsx
@@ -1,9 +1,11 @@
-import { useEffect } from 'react'
+import Link from 'next/link'
+import { useEffect, useState } from 'react'
 import useSWR, { mutate, SWRConfiguration } from 'swr'
 import { IUser } from '../library/schemas/interfaces'
 import useClientStore from '../library/stores/client'
 
 const ProjectForm = () => {
+  const [input, setInput] = useState('')
   const user = useClientStore((state) => state.user)
   const create = useClientStore((state) => state.create.project)
 
@@ -26,7 +28,7 @@ const ProjectForm = () => {
 
   const createHandler = async () => {
     await create({
-      name: 'Sample',
+      name: input,
       description: 'Sample',
       dueAt: 'September 22, 2022',
       userId: user.id,
@@ -38,10 +40,13 @@ const ProjectForm = () => {
 
   return (
     <div>
+      <input type="text" value={input} onChange={(event) => setInput(event.target.value)} />
       <button onClick={createHandler}>Add Project</button>
       <div>
         {user.members?.map((member) => (
-          <div key={member.id}>{member.project?.name}</div>
+          <Link key={member.id} href={`/connect/${user.id}/view/${member.id}/dashboard`}>
+            <div>{member.project?.name}</div>
+          </Link>
         ))}
       </div>
     </div>
