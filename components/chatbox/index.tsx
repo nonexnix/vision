ceb5ba5b3a880f6e6543d5a96a799653896fcc42
ntabucejo <ntabucejo@gmail.com>
2022-05-17 16:43:45 +0800

    Update to nested structure

---
+++ components/chatbox/index.tsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from 'react'
+import useSWR, { SWRConfiguration, mutate } from 'swr'
+import { IMessage } from '../../library/schemas/interfaces'
+import useClientStore from '../../library/stores/client'
+
+const Chatbox = () => {
+  const [input, setInput] = useState('')
+  const user = useClientStore((state) => state.user)
+  const project = useClientStore((state) => state.project)
+  const messages = useClientStore((state) => state.messages)
+  const create = useClientStore((state) => state.create.message)
+
+  const config: SWRConfiguration = {
+    fallbackData: messages,
+    refreshInterval: 100,
+  }
+
+  const fetcher = async (endpoint: string): Promise<IMessage[]> => {
+    return await fetch(endpoint, {
+      method: 'POST',
+      body: JSON.stringify({ projectId: project!.id }),
+    }).then((response) => response.json())
+  }
+
+  const { data } = useSWR<IMessage[]>('/api/message/read', fetcher, config)
+
+  useEffect(() => {
+    if (data! !== messages) {
+      useClientStore.getState().read.messages(data!)
+    }
+  }, [data])
+
+  const createHandler = async () => {
+    await create({
+      text: input,
+      memberId: user.members![0].id,
+      projectId: project.id,
+    })
+    mutate('/api/message/read')
+  }
+
+  return (
+    <div>
+      <h1>Chat</h1>
+      <input type="text" value={input} onChange={(event) => setInput(event.target.value)} />
+      <button onClick={createHandler}>Send Message</button>
+      <div>
+        {messages.map((message) => (
+          <div key={message.id}>{message.text}</div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Chatbox
