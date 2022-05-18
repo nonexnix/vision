a4e05633562b2a6db47b85fdfb9a78379e9fb78f
ntabucejo <ntabucejo@gmail.com>
2022-05-18 15:09:58 +0800

    Add swr mutate as second return

--- library/hooks/fetch.ts
+++ library/hooks/fetch.ts
@@ -2,8 +2,8 @@ import useSWR, { SWRConfiguration } from 'swr'
 import fetcher from '../utilities/fetcher'
 
 const useFetch = <Type>(key: string, id: string, options?: SWRConfiguration) => {
-  const { data } = useSWR<Type>({ key, id }, fetcher, options)
-  return { data }
+  const { data, mutate } = useSWR<Type>({ key, id }, fetcher, options)
+  return { data, mutate }
 }
 
 export default useFetch
