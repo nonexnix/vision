08c6e497c1a43786cee36431b4c4d575ffa811ac
ntabucejo <ntabucejo@gmail.com>
2022-05-18 13:19:05 +0800

    Remove layout

--- components/layout/index.tsx
+++
@@ -1,9 +0,0 @@
-interface IProps {
-  children: React.ReactNode
-}
-
-const Layout = ({ children }: IProps) => {
-  return <div className="min-h-screen grid grid-rows-[auto,1fr,auto]">{children}</div>
-}
-
-export default Layout
