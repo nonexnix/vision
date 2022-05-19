6219f116eeda735714d1c6bd161497b816256078
ntabucejo <ntabucejo@gmail.com>
2022-05-20 02:28:09 +0800

    Add create authorization api

---
+++ pages/api/authorization/create.ts
@@ -0,0 +1,28 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import phase from '../../../library/utilities/phase'
+import postman from '../../../library/utilities/postman'
+import prisma from '../../../library/utilities/prisma'
+
+type THandler = (request: NextApiRequest, response: NextApiResponse) => void
+
+const handler: THandler = async (request, response) => {
+  if (request.method === 'POST') {
+    const body = JSON.parse(request.body)
+    try {
+      await prisma.authorization.create({
+        data: {
+          memberId: body.memberId,
+          roleId: body.roleId,
+        },
+      })
+      response.status(201).json(postman(201))
+    } catch (error) {
+      console.error(error)
+      response.status(500).json(postman(500))
+    }
+  } else {
+    response.status(400).json(postman(400))
+  }
+}
+
+export default handler
