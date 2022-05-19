e553eb41b245033b24506a49cb9f975cbbd69060
ntabucejo <ntabucejo@gmail.com>
2022-05-20 02:27:54 +0800

    Add authorization interface

--- library/stores/client/interface.ts
+++ library/stores/client/interface.ts
@@ -26,6 +26,7 @@ interface ICreate {
   members: (payload: { userId: string; projectId: string }[]) => void
   project: (payload: { name: string; description: string; dueAt: string; userId: string }) => void
   role: (payload: { name: string; description: string; projectId: string }) => void
+  authorization: (payload: { memberId: string; roleId: string }) => void
   message: (payload: { text: string; memberId: string; projectId: string }) => void
   reaction: (payload: { emoji: TEmoji; memberId: string; messageId: string }) => void
   task: (payload: {
