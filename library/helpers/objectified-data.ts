c6963cd22a74bb9dae34fec1043a6498b693dd4f
ntabucejo <ntabucejo@gmail.com>
2022-06-01 00:05:19 +0800

    Change to serialized data

--- library/helpers/objectified-data.ts
+++
@@ -1,3 +0,0 @@
-const objectifiedData = (data: any) => JSON.parse(JSON.stringify(data))
-
-export default objectifiedData
---
+++ library/helpers/serialize-data.ts
@@ -0,0 +1,3 @@
+const serializeData = (data: any) => JSON.parse(JSON.stringify(data))
+
+export default serializeData
