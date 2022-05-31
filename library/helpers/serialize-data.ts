263ed2a47d5a5da52dd803636e14df4af0e06a18
ntabucejo <ntabucejo@gmail.com>
2022-06-01 01:35:46 +0800

    Update type

--- library/helpers/serialize-data.ts
+++ library/helpers/serialize-data.ts
@@ -1,3 +1,11 @@
-const serializeData = (data: any) => JSON.parse(JSON.stringify(data))
+import { Prisma, User } from '@prisma/client'
+
+type Data =
+  | Prisma.UserInclude
+  | Prisma.MemberInclude
+  | Prisma.ProjectInclude
+  | Prisma.MessageInclude[]
+
+const serializeData = (data: Data | User[]) => JSON.parse(JSON.stringify(data))
 
 export default serializeData
