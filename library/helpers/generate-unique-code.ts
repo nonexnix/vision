0c2735e4597518731c33faddc8a94c63cecc39dc
ntabucejo <ntabucejo@gmail.com>
2022-06-01 15:44:45 +0800

    Add return type

--- library/helpers/generate-unique-code.ts
+++ library/helpers/generate-unique-code.ts
@@ -1,6 +1,6 @@
 import cryptoRandomString from 'crypto-random-string'
 
-const generateUniqueCode = () => {
+const generateUniqueCode = (): string => {
   const one = cryptoRandomString({ length: 4, type: 'distinguishable' })
   const two = cryptoRandomString({ length: 4, type: 'distinguishable' })
   const three = cryptoRandomString({ length: 4, type: 'distinguishable' })
